2e26b623489564d6a7e2de0467032f2fa942464f
nk0086 <n172586k@gmail.com>
2023-01-03 11:48:35 +0900

    情報を登録する機能を追加

---
+++ database.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Organism struct {
+	ID    int64
+	Name  string
+	Image []byte
+	Lat   float64
+	Lng   float64
+}
+
+func connectToDatabase(filename string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS organisms (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	image BLOB,
+	lat REAL,
+	lng REAL
+	)`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func insertIntoTable(db *sql.DB, name string, image []byte, lat float64, lng float64) (int64, error) {
+	result, err := db.Exec(`INSERT INTO organisms (name, image, lat, lng) VALUES (?, ?, ?, ?)`, name, image, lat, lng)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return result.LastInsertId()
+}
+
+func selectAllFromTable(db *sql.DB) ([]*Organism, error) {
+	// Select all rows from the table
+	rows, err := db.Query(`SELECT id, name, image, lat, lng FROM organisms`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var organisms []*Organism
+	for rows.Next() {
+		var id int64
+		var name string
+		var image []byte
+		var lat float64
+		var lng float64
+		if err := rows.Scan(&id, &name, &image, &lat, &lng); err != nil {
+			return nil, err
+		}
+		organisms = append(organisms, &Organism{
+			ID:    id,
+			Name:  name,
+			Image: image,
+			Lat:   lat,
+			Lng:   lng,
+		})
+	}
+
+	return organisms, rows.Err()
+}
--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"example.com/bio-map/oauth"
 )
 
 type Page struct {
-	Title string
-	Body  string
+	Title     string
+	Body      string
+	Organisms []*Organism
 }
 
 var templates = make(map[string]*template.Template)
@@ -25,6 +28,10 @@ func main() {
 	// 生物の情報を登録
 	templates["register"] = loadTemplate("register")
 	http.HandleFunc("/register", handleRegister)
+
+	// 生物の情報を一覧表示
+	templates["list"] = loadTemplate("list")
+	http.HandleFunc("/list", handleList)
 
 	http.HandleFunc("/login", oauth.LoginHandler)
 	http.HandleFunc("/about", oauth.CallbackHandler)
@@ -44,6 +51,31 @@ func loadTemplate(name string) *template.Template {
 	return tmpl
 }
 
+func handleList(w http.ResponseWriter, r *http.Request) {
+	db, err := connectToDatabase("database.db")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	defer db.Close()
+	organisms, err := selectAllFromTable(db)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	page := Page{
+		Title:     "BioMap",
+		Body:      "This is a test",
+		Organisms: organisms,
+	}
+
+	if err := templates["list"].Execute(w, page); err != nil {
+		log.Printf("failed to execute template: %v", err)
+	}
+}
+
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	page := Page{
 		Title: "BioMap",
@@ -56,13 +88,59 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRegister(w http.ResponseWriter, r *http.Request) {
-	page := Page{
-		Title: "BioMap",
-		Body:  "This is a test",
-	}
+	//生物情報を登録する
+	if r.Method == "POST" {
+		if err := r.ParseMultipartForm(1024); err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
 
-	if err := templates["register"].Execute(w, page); err != nil {
-		log.Printf("failed to execute template: %v", err)
+		name := r.FormValue("name")
+		latitude, _ := strconv.ParseFloat(r.FormValue("latitude"), 64)
+		longitude, err := strconv.ParseFloat(r.FormValue("longitude"), 64)
+		if err != nil {
+			log.Printf("failed to parse float: %v", err)
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		file, _, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		defer file.Close()
+
+		image, err := ioutil.ReadAll(file)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		db, err := connectToDatabase("database.db")
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		defer db.Close()
+
+		_, err = insertIntoTable(db, name, image, latitude, longitude)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		http.Redirect(w, r, "/register", http.StatusFound)
+
+	} else {
+		page := Page{
+			Title: "BioMap",
+			Body:  "This is a test",
+		}
+
+		if err := templates["register"].Execute(w, page); err != nil {
+			log.Printf("failed to execute template: %v", err)
+		}
 	}
 
 }
